6bb0a22fc6ddfc95b65c7daf39742690cc613363
michaeljamesrojas <michaeljamesrojas123@gmail.com>
2024-10-12 22:46:17 +0800

    so far

---
+++ index-temporary.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the alias name and the URL of the globally.sh script
+ALIAS_NAME="globally"
+SCRIPT_URL="https://raw.githubusercontent.com/michaeljamesrojas/scripts/main/globally.sh"
+
+# Add the alias to the current shell session
+add_alias_temp() {
+    alias $ALIAS_NAME="bash <(curl -s $SCRIPT_URL)"
+    echo "Temporary alias '$ALIAS_NAME' has been added to the current shell session."
+}
+
+# Main execution
+add_alias_temp
