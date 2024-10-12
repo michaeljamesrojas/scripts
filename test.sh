33482253d876f3e8882b3e1a58ec81600241dcc4
michaeljamesrojas <michaeljamesrojas123@gmail.com>
2024-10-12 14:26:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
-read -p "Install Neovim first? (y/n): " answer
+# read -p "Install Neovim first? (y/n): " answer
 
-if [ "$answer" = "y" ]; then
-    sudo apt install neovim
-fi
+# if [ "$answer" = "y" ]; then
+#     sudo apt install neovim
+# fi
+
+
+read -p "Install Neovim first? (y/n): "
