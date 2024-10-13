c4f6366a2e8bb03d3bc8ec5b30461a935d379f7e
Michael James Rojas <67911561+michaeljamesrojas@users.noreply.github.com>
2024-10-13 19:29:10 +0000

    so far

--- index.sh
+++ index.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Define the alias name and the URL of the globally.sh script
-ALIAS_NAME="ggg"
+ALIAS_NAME1="ggg"
+ALIAS_NAME2="globally"
+
 # SCRIPT_URL="https://raw.githubusercontent.com/michaeljamesrojas/scripts/main/globally.sh?token=$(date +%s)"
 SCRIPT_URL="https://raw.githubusercontent.com/michaeljamesrojas/scripts/main/globally.sh"
 
 # Add the alias to the user's shell configuration file
 add_alias() {
+    local alias_name="$1"
     local shell_rc="$HOME/.$(basename $SHELL)rc"
-    echo "alias $ALIAS_NAME='bash <(curl -s $SCRIPT_URL)'" >> "$shell_rc"
-    echo "Alias '$ALIAS_NAME' has been added to $shell_rc"
+    echo "alias $alias_name='bash <(curl -s $SCRIPT_URL)'" >> "$shell_rc"
+    echo "Alias '$alias_name' has been added to $shell_rc"
 }
 
+
 # Main execution
-add_alias
+add_alias "$ALIAS_NAME1"
+add_alias "$ALIAS_NAME2"
 echo "Please restart your terminal or run 'source $HOME/.$(basename $SHELL)rc' to use the new alias."
