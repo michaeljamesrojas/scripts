93cf092aeb321ce71f0b83e40e6ba8ff06d59e40
Michael James Rojas <67911561+michaeljamesrojas@users.noreply.github.com>
2024-10-13 15:15:32 +0000

    so far

--- test.sh
+++ test.sh
@@ -3,5 +3,8 @@
 # @echo off
 # start chrome --profile-directory="Person 1" %*
 
+
 # Start Chrome with the "Person 1" profile and pass all arguments
+echo opening chrome with the link provided
+
 start chrome --profile-directory="Person 1" "$@"
