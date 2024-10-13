13d2406077480c85b8ff4237445caf002e3f2242
Michael James Rojas <67911561+michaeljamesrojas@users.noreply.github.com>
2024-10-14 02:59:27 +0800

    Edit editscript

--- editscripts.sh
+++ editscripts.sh
@@ -3,5 +3,12 @@
 echo "VISIT THIS LINK"
 echo
 
-echo "https://animated-yodel-46vgg9rr7wgf7r5v.github.dev/"
+echo "Github"
 echo "https://github.com/michaeljamesrojas/scripts"
+echo
+echo "Codespace"
+echo "https://animated-yodel-46vgg9rr7wgf7r5v.github.dev/"
+echo
+echo "Vscode Dev"
+echo "https://github.dev/michaeljamesrojas/scripts"
+
