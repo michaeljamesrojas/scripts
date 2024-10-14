442d59fb93c4330a87e05fd3f768c5981ccf77dc
Michael James Rojas <67911561+michaeljamesrojas@users.noreply.github.com>
2024-10-14 14:45:29 +0000

    Revert "so far"
    This reverts commit a8980450fbe367c0f8c0ab7712d13e8d0550d73b.

--- last-2-clipbard-contents.sh
+++ last-2-clipbard-contents.sh
@@ -8,13 +8,26 @@ get_clipboard() {
 # Array to store clipboard history
 clipboard_history=()
 
-# Get the last 2 clipboard contents
-for i in {1..2}; do
-    content=$(get_clipboard)
-    clipboard_history+=("$content")
-    # Use PowerShell to clear the clipboard
-    powershell.exe -command "Set-Clipboard -Value ''"
-done
+# Get initial clipboard content
+initial_content=$(get_clipboard)
+clipboard_history+=("$initial_content")
+
+# Wait for a short time and get the content again
+sleep 1
+current_content=$(get_clipboard)
+
+# Add the current content if it's different from the initial
+if [ "$current_content" != "$initial_content" ]; then
+    clipboard_history+=("$current_content")
+else
+    # If it's the same, try to get a different content
+    echo "Copy something new to your clipboard..."
+    while [ "$current_content" == "$initial_content" ]; do
+        sleep 1
+        current_content=$(get_clipboard)
+    done
+    clipboard_history+=("$current_content")
+fi
 
 echo "Last 2 clipboard contents:"
 for i in "${!clipboard_history[@]}"; do
