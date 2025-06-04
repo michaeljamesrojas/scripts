a1243630f480dccbbd39048df87c1ec5d1885d79
michaeljamesrojas <michaeljamesrojas123@gmail.com>
2025-06-04 20:47:27 +0800

    Add git-stash-saveall.sh via git-clone-add-push script at 2025-06-04 20:47:27

---
+++ git-stash-saveall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if a message argument is provided
+if [ -z "$1" ]; then
+  echo "Error: No stash message provided."
+  exit 1
+fi
+
+# Run git stash save -u with the provided message
+git stash save -u "$1"
