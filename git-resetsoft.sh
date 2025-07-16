200c5bf3f459adfad85530d8e6d0fc64d5d0eb97
michaeljamesrojas <michaeljamesrojas123@gmail.com>
2025-07-16 16:29:30 +0800

    git reset

--- git-resetsoft.sh
+++ git-resetsoft.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Ask user for number of commits to reset from HEAD
-read -p "Enter how many commits to reset hard from HEAD~: " count
+read -p "Enter how many commits to reset soft from HEAD~: " count
 
 # Confirm before running the reset
-echo "You are about to run: git reset --hard HEAD~$count"
+echo "You are about to run: git reset --soft HEAD~$count"
 read -p "Are you sure? (y/N): " confirm
 
 if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-    git reset --hard HEAD~"$count"
+    git reset --soft HEAD~"$count"
 else
     echo "Aborted."
 fi
